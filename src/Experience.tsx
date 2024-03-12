89b4e06644a9fcc90a772240fa206abd35e5de8b
TimeLightCurve <146640416+TimeLightCurve@users.noreply.github.com>
2024-03-12 17:03:28 +0330

    Update Experience.tsx

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -20,7 +20,7 @@ function Experience(props :prop) {
     <>
     <div className=" w-screen h-screen fixed z-0 ">
       <Canvas
-        className="h-screen w-screen bg-black"
+        className="h-screen w-screen"
         gl={{
           powerPreference: "high-performance",
           toneMapping: THREE.NoToneMapping,
